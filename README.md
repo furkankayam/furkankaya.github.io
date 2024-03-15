761ab20c9ba872248f9657a0d5877d7ed764dc9b
Furkan Kaya <furkan.36kaya@gmail.com>
2024-03-15 20:30:57 +0300

    feat: update website

---
+++ README.md
@@ -0,0 +1,20 @@
+# My Website
+
+<br>
+
+### Light Theme
+
+![[Light Theme]](./images/demo1.png)
+
+<br>
+
+### Dark Theme
+
+![[Dark Theme]](./images/demo2.png)
+
+<br>
+
+# License
+
+This project is licensed under the MIT License.
+See the [LICENSE](LICENSE) file for details
---
+++ script.js
@@ -0,0 +1,95 @@
+const toggle = document.querySelector(".toggle__theme");
+const card = document.querySelector(".app");
+toggle.addEventListener("click", () => {
+    let theme = toggle.querySelector(".fas");
+    if (theme.classList.contains("fa-moon")) {
+        theme.classList.remove("fa-moon");
+        theme.classList.add("fa-sun");
+        card.classList.add("dark");
+    } else {
+
+        theme.classList.remove("fa-sun");
+        theme.classList.add("fa-moon");
+        card.classList.remove("dark");
+
+    }
+})
+
+function killCopy(e){
+    return false;
+}
+
+function reEnable(){
+    return true;
+}
+
+document.onselectstart=new Function ("return false");
+
+if (window.sidebar){
+    document.onmousedown=killCopy;
+    document.onclick=reEnable;
+}
+
+function noteOut()
+{
+    var note = document.querySelector(".note");
+    note.style.display = "none";
+}
+
+setInterval(noteOut,3000);
+
+window.onload = function() {
+    document.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function(e) {
+        //document.onkeydown = function(e) {
+        // "I" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+            disabledEvent(e);
+        }
+        // "J" key
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+            disabledEvent(e);
+        }
+        // "S" key + macOS
+        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+            disabledEvent(e);
+        }
+        // "U" key
+        if (e.ctrlKey && e.keyCode == 85) {
+            disabledEvent(e);
+        }
+        // "F12" key
+        if (event.keyCode == 123) {
+            disabledEvent(e);
+        }
+    }, false);
+
+    function disabledEvent(e) {
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        } else if (window.event) {
+            window.event.cancelBubble = true;
+        }
+        e.preventDefault();
+        return false;
+    }
+};
+
+// Ctrl + U
+document.onkeydown = function(e) {
+    if (e.ctrlKey && 
+        (e.keyCode === 67 || 
+         e.keyCode === 86 || 
+         e.keyCode === 85 || 
+         e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
+$(document).keypress("u",function(e) {
+    if(e.ctrlKey) return false;
+    else return true;
+});
